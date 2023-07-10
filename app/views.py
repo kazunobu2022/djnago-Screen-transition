d6b13d49e0141a84c4d34cbe6c208c130b4130eb
Your Name <you@example.com>
2023-07-10 17:08:37 +0900

    成功

--- app/views.py
+++ app/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def frontpage(request):
     return render(request, "app/forntpage.html")
+
+def frontpage2(request):
+    return render(request, "app/forntpage2.html")
--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path
 
 from app.views import frontpage
+from app.views import frontpage2
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',frontpage),
+    path('',frontpage, name='confirm'),
+    path('test',frontpage2, name='confirm2'),
 ]
